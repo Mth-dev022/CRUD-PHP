c6dba41f4f3f0f796a6844000a8b07643d379413
Matheus Souza <Matheuszebende6@gmail.com>
2023-08-08 23:13:37 -0300

    Add files via upload

---
+++ tarefa_controller.php
@@ -0,0 +1,116 @@
+<?php 
+
+require "tarefa.model.php";
+require "tarefa.service.php";
+require "conexao.php";
+
+$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+
+if ($acao == 'inserir') {
+
+$tarefa = new Tarefa();
+
+$tarefa->__set('tarefa', $_POST['tarefa']);
+
+$conexao = new Conexao();
+
+$tarefaService = new tarefa_service($conexao, $tarefa);
+
+$tarefaService->inserir();
+
+header('location:nova_tarefa.php?inclusao=1');
+
+} else if ($acao == 'recuperar') {
+
+     $tarefa = new Tarefa();
+
+     $conexao = new Conexao();
+
+     $tarefaService = new tarefa_service($conexao, $tarefa);
+
+     $tarefas = $tarefaService->recuperar();
+
+} else if ($acao =='atualizar') {
+     
+    $tarefa = new Tarefa();
+    
+    $tarefa->__set('id', $_POST['id']);
+   
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+    
+    $conexao = new Conexao();
+
+    $tarefaService = new tarefa_service($conexao, $tarefa);
+
+    if ($tarefaService->atualizar()) {
+
+        if (isset ($_GET['pag']) && $_GET['pag'] == 'index') {
+            
+            header('location: index.php');
+        }
+
+       else {
+
+        header('location: todas_tarefas.php');
+    }
+
+    $tarefaService->atualizar();
+    
+}
+
+   
+
+} else if ($acao =='remover') {
+
+    $tarefa = new Tarefa();
+    $tarefa->__set('id', $_GET['id']);
+    $conexao = new Conexao();
+    $tarefaService = new tarefa_service($conexao, $tarefa);
+    $tarefaService->remover();
+
+    if (isset ($_GET['pag']) && $_GET['pag'] == 'index') {
+            
+        header('location: index.php');
+    }
+
+   else {
+
+    header('location: todas_tarefas.php');
+}
+
+} else if ($acao == 'realizar') {
+
+    $tarefa = new Tarefa();
+    $tarefa->__set('id', $_GET['id']);
+    $tarefa->__set('id_status', 2);
+    $conexao = new Conexao();
+    $tarefaService = new tarefa_service($conexao, $tarefa);
+    $tarefaService->realizar();
+
+    if (isset ($_GET['pag']) && $_GET['pag'] == 'index') {
+            
+        header('location: index.php');
+    }
+
+   else {
+
+    header('location: todas_tarefas.php');
+}
+ 
+}
+
+else if($acao='TarefasPendi') {
+
+    $tarefa = new Tarefa();
+
+    $tarefa->__set('id_status', 1);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new tarefa_service($conexao, $tarefa);
+
+    $tarefas = $tarefaService->tarefasPendi();
+
+}
+
+?>  
